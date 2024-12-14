bd7929894514fedaa130e84b0f75ea34134138dc
stefv21 <161451930+stefv21@users.noreply.github.com>
2024-12-14 07:17:10 -0500

    updated async wait

--- index.js
+++ index.js
@@ -22,25 +22,18 @@ app.get('/example', (req, res) => {
 });
 
 
-//mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
+(async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION_URI, { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true
+    });
+    console.log('Database connected');
+  } catch (err) {
+    console.error('Database connection error:', err);
+  }
+})();
 
-//mongoose.connect('mongodb+srv://svargas1121:make120k@mymovieapi.tx01r.mongodb.net/MyMovieApi?retryWrites=true&w=majority&appName=MyMovieApi', 
-//{ 
- //useNewUrlParser: true, 
-  //useUnifiedTopology: true });
-
-
-mongoose.connect(process.env.CONNECTION_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-})
-
-.then(() => {
-  console.log('Database connected');
-})
-.catch((err) => {
-  console.error('Database connection error:', err);
-});
 
 
 
