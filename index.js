586e0ae40792cc6c6e295d7263c0c448aae39d4f
stefv21 <161451930+stefv21@users.noreply.github.com>
2025-01-02 19:18:23 -0500

    Moved auth require after movie routes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/test', (req, res) => {
 });
 
 
-require('./auth')(app);
+
 
 app.use(cors());
 
@@ -304,3 +304,5 @@ const port = process.env.PORT || 8080;
 app.listen(port,() => {
  console.log('Listening on Port ' + port);
 });
+
+require('./auth')(app);
