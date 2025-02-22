e64d421cff6398ce0bfa7fa528685b50d7bb9b1c
stefv21 <161451930+stefv21@users.noreply.github.com>
2025-02-22 16:57:47 -0500

    added cors allowed origins

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const passport = require('passport');
 const express = require('express');
 const morgan = require('morgan'); 
 const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234',
+  'https://myflixapp-0225.netlify.app'];
+
 const app = express();
 const { check, validationResult } = require('express-validator');
 
@@ -41,7 +44,16 @@ const corsOptions = {
 };
 
 
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 
 
